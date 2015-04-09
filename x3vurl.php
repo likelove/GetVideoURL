8de5b2a129f4cf7d4573099c3b715e873e35e025
likelove <likelove@users.noreply.github.com>
2015-04-09 16:37:28 +0800

    Update x3vurl.php

--- x3vurl.php
+++ x3vurl.php
@@ -1,17 +1,33 @@
 <?php
+//tb8,tbc必须准确的传入附加参数才能获取网页,未处理
+//http://www.tube8.com/hardcore/jayden-jaymes%2C-juctice%2C-jon-jon-creampie/19243132/
+//http://www.tubecup.com/videos/141621/real-sex-real-joy/
 	//处理传入参数
 	if(isset($_GET['h'],$_GET['c'])){
-		if($_GET['h']==='xhm') {
+		switch(strtolower($_GET['h'])){
+		case "xhm":
 			$xurl='http://xhamster.com/movies/'.$_GET['c'].'/0_1_2.html';
-		}
-		elseif($_GET['h']==='rtb') {
+			break;
+		case "rtb":
 			$xurl='http://www.redtube.com/'.$_GET['c'];
-		}
-		else{
+			break;
+		case "mlt";
+			$xurl='http://mylust.com/videos/'.$_GET['c'].'/0_1_2/';
+			break;
+		case "tb8";
+			$xurl='http://www.tube8.com/0_1_2/3_4_5/'.$_GET['c'].'/';
+			break;
+		case "tbc";
+			$xurl='http://www.tubecup.com/videos/'.$_GET['c'].'/0_1_2/';
+			break;
+		case "ypn";
+			$xurl='http://www.youporn.com/watch/'.$_GET['c'].'/0_1_2/';
+			break;
+		default:
 			echo '未被支持的网络主机';
 			die();
 		}
-		if (!preg_match('/[0-9]{5,8}/', $_GET['c'])) {
+		if (!preg_match('/[0-9]{5,9}/', $_GET['c'])) {
 			echo '未被支持的特征码';
 			die();
 		}
@@ -62,6 +78,58 @@
 				preg_match('/p=(.+?)&embedCode=%3Ciframe/',$data,$flvurladd);
 				$flvadd=rawurldecode($flvurladd[1]);
 				unset($flvurladd);
+			}
+			//处理mylust
+			elseif($_GET['h']==='mlt') {
+				preg_match('%<title>(.+?) - MyLust.com</title>%',$data,$name);
+				$title=$name[1];
+				unset($name);
+				preg_match("%video_url: '(.+?)',%",$data,$mp4urladd);
+				$mp4add=$mp4urladd[1];
+				unset($mp4urladd);
+				preg_match("%preview_url: '(.+?)',%",$data,$picurladd);
+				$picadd=$picurladd[1];
+				unset($picurladd);
+				$flvadd='';
+			}
+			//处理tube8
+			elseif($_GET['h']==='tb8') {
+				preg_match('%videotitle = "(.+?)",%',$data,$name);
+				$title=$name[1];
+				unset($name);
+				preg_match("/videoUrlJS = '(.+?)',/",$data,$mp4urladd);
+				$mp4add=$mp4urladd[1];
+				unset($mp4urladd);
+				preg_match('/"image_url":"(.+?)",/',$data,$picurladd);
+				$picadd=str_replace('\/','/',$picurladd[1]);
+				unset($picurladd);
+				$flvadd='';
+			}
+			//处理tubecup
+			elseif($_GET['h']==='tbc') {
+				preg_match('%<title>(.+?) | Tube Cup</title>%',$data,$name);
+				$title=$name[1];
+				unset($name);
+				preg_match("/video_url: '(.+?)',/",$data,$mp4urladd);
+				$mp4add=$mp4urladd[1];
+				unset($mp4urladd);
+				preg_match("%preview_url: '(.+?)',%",$data,$picurladd);
+				$picadd=$picurladd[1];
+				unset($picurladd);
+				preg_match("/video_alt_url: '(.+?)',/",$data,$flvurladd);
+				$flvadd=$flvurladd[1];
+				unset($flvurladd);
+			}
+			//处理youporn
+			elseif($_GET['h']==='ypn') {
+				preg_match('%<title>(.+?) - Free Porn Videos - YouPorn</title>%',$data,$name);
+				$title=$name[1];
+				unset($name);
+				preg_match('%<video id="player-html5" src="(.+?)" x-webkit-airplay="allow" autobuffer controls poster="(.+?)" width="600" height="500">%',$data,$mp4urladd);
+				$mp4add=$mp4urladd[1];
+				$picadd=$mp4urladd[2];
+				unset($mp4urladd);
+				$flvadd='';
 			}
 			//输出json
 			$arr=array ('host'=>$_GET['h'],'code'=>$_GET['c'],'title'=>$title,'mp4'=>$mp4add,'flv'=>$flvadd,'pic'=>$picadd);
